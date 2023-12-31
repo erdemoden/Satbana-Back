85042a99bb5ffb39887cee63639b1f77719aec3b
erdemoden <erdemoden5@gmail.com>
2024-01-01 01:35:18 +0300

    redis entegrasyon

--- mailservice/src/main/java/com/woh/mailservice/Service/Mailimtl/MailserviceImtl.java
+++ mailservice/src/main/java/com/woh/mailservice/Service/Mailimtl/MailserviceImtl.java
@@ -1,8 +1,11 @@
 package com.woh.mailservice.Service.Mailimtl;
 
 import com.woh.mailservice.Service.MailService;
+import com.woh.mailservice.redis.RedisStore;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -13,29 +16,37 @@ import java.util.Random;
 @Service
 public class MailserviceImtl implements MailService {
     private JavaMailSender Mailsender;
-
-    public MailserviceImtl(JavaMailSender mailsender) {
+    private RedisStore Redisstore;
+    public MailserviceImtl(JavaMailSender mailsender,RedisStore redisStore) {
         Mailsender = mailsender;
+        Redisstore = redisStore;
     }
 
 
     @Override
-    public String sendMail() {
+    public String sendMail(String mail) {
         String verificationCode = generateVerificationCode();
         MimeMessage mimeMessage = Mailsender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
-
         try {
             messageHelper.setFrom("serkafidanci@gmail.com");
-            messageHelper.setTo("erdemoden5@gmail.com");
+            messageHelper.setTo(mail);
             messageHelper.setSubject("Doğrulama Kodu");
             messageHelper.setText("<h1>Doğrulama Kodunuz: " + verificationCode + "</h1>", true);
             Mailsender.send(mimeMessage);
-            return verificationCode;
+            Redisstore.put(verificationCode,mail);
+            return "Doğrulama Kodu Başarıyla Gönderildi";
         } catch (MessagingException e) {
             e.printStackTrace();
             return "Doğrulama Kodunda Hata Oluştu";
         }
+    }
+    @Override
+    public Boolean checkMail(String key,String mail){
+        if( Redisstore.getValue(key)!=null && Redisstore.getValue(key).equals(mail)){
+            return true;
+        }
+        return false;
     }
     public String generateVerificationCode() {
         Random random = new Random();
--- satbana/src/main/java/com/woh/satbana/Services/UserService.java
+++ satbana/src/main/java/com/woh/satbana/Services/UserService.java
@@ -3,24 +3,30 @@ package com.woh.satbana.Services;
 import com.woh.satbana.DTO.CreateUserDTO;
 import com.woh.satbana.Entities.User;
 import com.woh.satbana.Repositories.UserRepository;
+import com.woh.satbana.Responses.ErrorSuccess;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class UserService {
 private final UserRepository userRepository;
+private final JWTService jwtService;
+private final ModelMapper mapper;
+public ErrorSuccess saveUserCreateJWT(CreateUserDTO createUserDTO){
+    ErrorSuccess errorSuccess = new ErrorSuccess();
+    if(!checkUser(createUserDTO)){
+        errorSuccess.setSuccess("Please check your email and enter the 6-digit number");
 
-public void saveUser(CreateUserDTO createUserDTO){
-    if(checkUser(createUserDTO)){
-
+        return errorSuccess;
     }
-    User user = new User();
-    user.setEmail(createUserDTO.getEmail());
-    user.setUsername(createUserDTO.getUsername());
-    userRepository.save(user);
+    errorSuccess.setError("The user is already registered in the system. Please try to log in.");
+    return errorSuccess;
 }
 public boolean checkUser(CreateUserDTO createUserDTO){
     User user = null;
